dd225f840d21d1a5392476bb3f1320f1b8226e40
330311 <330311@uwr.edu.pl>
2022-12-18 21:18:14 +0100

    kek

--- jobs.c
+++ jobs.c
@@ -271,12 +271,11 @@ int monitorjob(sigset_t *mask) {
   /* TODO: Following code requires use of Tcsetpgrp of tty_fd. */
 #ifdef STUDENT
   sigprocmask(SIG_SETMASK, mask, NULL);
-  // sigsuspend(mask);
+  
   state = jobstate(0, &exitcode);
   // fprintf(stderr,"%d?!\n",state);
-  while (state == RUNNING) { // czekamy, aż proces z foregroundu się skończy
-                             // fprintf(stderr,"%d?!\n",state);
-                             // obsługujemy sygnały
+  while (state == RUNNING) { 
+    sigsuspend(mask);
     state = jobstate(0, &exitcode);
   }
   if (state == STOPPED)
